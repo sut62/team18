d5ec31225e709ccb5c1e7483816dd60d1f9251c5
Sarinya Kotphong <frong8173@gmail.com>
2020-01-07 00:45:25 +0700

    close #78 - add file ShowRepository

---
+++ backend/src/main/java/com/okta/springbootvue/repository/ShowRepository.java
@@ -0,0 +1,12 @@
+package com.okta.springbootvue.repository;
+
+import com.okta.springbootvue.entity.Show;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public
+interface ShowRepository extends JpaRepository<Show, Long> {
+
+}
