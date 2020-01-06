fceadcad1e567a587f7b3a4ab053e242430eca1b
titlemarek <tiger.ok123@gmail.com>
2020-01-06 21:13:11 +0700

    close #65 - add file ShowLocationController

---
+++ backend/src/main/java/com/okta/springbootvue/controller/ShowLocationController.java
@@ -0,0 +1,33 @@
+package com.okta.springbootvue.controller;
+
+import com.okta.springbootvue.entity.ShowLocation;
+import com.okta.springbootvue.repository.ShowLocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class ShowLocationController {
+
+    @Autowired
+    private final ShowLocationRepository showlocationRepository;
+
+    public ShowLocationController(ShowLocationRepository showlocationRepository) {
+        this.showlocationRepository = showlocationRepository;
+    }
+
+    @GetMapping("/showlocation")
+    public Collection<ShowLocation> Shows() {
+        return showlocationRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+ 
+
+}
