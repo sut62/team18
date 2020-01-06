a0a43d5b2aac7a04fd98ced11770b56dd1508295
titlemarek <tiger.ok123@gmail.com>
2020-01-06 20:24:17 +0700

    close #58 - add file entity Time

---
+++ backend/src/main/java/com/okta/springbootvue/entity/Time.java
@@ -0,0 +1,29 @@
+package com.okta.springbootvue.entity;
+import lombok.*;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="TIME")
+public class Time {
+    @Id
+    @SequenceGenerator(name="TIME_SEQ",sequenceName="TIME_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="TIME_SEQ")
+    @Column(name="TIME_ID",unique = true, nullable = true)
+    private @NonNull Long id;
+
+    private @NonNull String time;
+
+	public void setTime(String name) {
+        this.time = name;
+	}
+
+ 
+}
