466c0d5f9995fd90e45e4fc37b7326ed198becf3
titlemarek <tiger.ok123@gmail.com>
2020-01-07 20:38:09 +0700

    close #33 - add file UI Showtime

---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-app-bar app class="blue darken-3"
+      >
+      <v-toolbar-title class="headline text-uppercase">
+        
+        <span class="font-weight-light white--text"> ระบบจองตั๋วการแสดง</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Username : {{username}}</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content class="blue lighten-3">
+     
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    username: '',
+  }),
+};
+</script>
