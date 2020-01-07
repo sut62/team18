9e811557757d898d477afbbe1fe93030a3431fa6
kittichai-6001520 <mosmos11289@gmail.com>
2020-01-07 19:28:29 +0700

    close #95 - add file SexController สำหรับระบบสมัครสมาชิก

---
+++ backend/src/main/java/com/okta/springbootvue/controller/SexController.java
@@ -0,0 +1,30 @@
+package com.okta.springbootvue.controller;
+import com.okta.springbootvue.entity.Sex;
+import com.okta.springbootvue.repository.SexRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class SexController {
+ @Autowired
+    private final SexRepository sexRepository;
+
+    public SexController(SexRepository sexRepository) {
+        this.sexRepository = sexRepository;
+    }
+
+    @GetMapping("/sex")
+    public Collection<Sex> Sex() {
+        return sexRepository.findAll().stream().collect(Collectors.toList());
+    } 
+
+    
+}
+
