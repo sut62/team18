f8289a7acc8a41ebf66955dfd665e0c703061303
titlemarek <tiger.ok123@gmail.com>
2020-01-09 04:41:54 +0700

    close #124 - fix file Sex.java

--- backend/src/main/java/com/okta/springbootvue/entity/Sex.java
+++ backend/src/main/java/com/okta/springbootvue/entity/Sex.java
@@ -24,9 +24,9 @@ public class Sex {
     private @NonNull Long id;
     private @NonNull String sex;
 
-    @OneToMany(fetch = FetchType.EAGER)
+    //@OneToMany(fetch = FetchType.EAGER)
     
-    private Collection<UserRegister> userregister;
+    //private Collection<UserRegister> userregister;
 
     public void setName(String name) {
 		this.sex = name;
