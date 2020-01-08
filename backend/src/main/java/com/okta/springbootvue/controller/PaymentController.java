090e2f8dbc246060882ab169e1d4dfae8cf1ca37
Papatchanan Khamthanate <aom9love9snsd@gmail.com>
2020-01-08 23:44:53 +0700

    close #117 - add file PaymentController

---
+++ backend/src/main/java/com/okta/springbootvue/controller/PaymentController.java
@@ -0,0 +1,32 @@
+package com.okta.springbootvue.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import com.okta.springbootvue.entity.Payment;
+import com.okta.springbootvue.repository.PaymentRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class PaymentController {
+
+    @Autowired
+    private final PaymentRepository paymentRepository;
+
+    public PaymentController(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    @GetMapping("/payment")
+    public Collection<Payment> Payments() {
+        return paymentRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+}
